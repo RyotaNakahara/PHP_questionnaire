36d6a6a2555c9b0953aee5ef1e89f1b9da7b89db
RyotaNakahara <rita.n4210@gmail.com>
2024-02-12 14:16:56 +0900

    ログイン保持

--- php/model/user.model.php
+++ php/model/user.model.php
@@ -1,10 +1,11 @@
 <?php
 namespace model;
 
-class UserModel {
+class UserModel extends AbstractModel {
     public string $id;
     public string $pwd;
     public string $nickname;
     public int $del_flg;
     
+    protected static $SESSION_NAME = '_user';
 }
