7016b16e04f12a0b27de840ac02f5c3a5b38a889
Ryota Nakahara <ryota@R2024.local>
2024-02-13 18:54:36 +0900

    リダイレクト、例外処理とエラーメッセージ追加

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
-<?php 
+<?php
+
+use lib\Msg;
+
 require_once 'config.php';
 
 // Library
 require_once SOURCE_BASE . 'libs/helper.php';
 require_once SOURCE_BASE . 'libs/auth.php';
+require_once SOURCE_BASE . 'libs/router.php';
 
 // model
 require_once SOURCE_BASE . 'model/abstract.model.php';
 require_once SOURCE_BASE . 'model/user.model.php';
+
+// Message
+require_once SOURCE_BASE . 'libs/message.php';
 
 // db
 require_once SOURCE_BASE . 'db/datasource.php';
@@ -17,6 +24,7 @@ require_once SOURCE_BASE . 'db/user.query.php';
 // $result = UserQuery::fetchById('test');
 // var_dump($result);
 
+use function lib\route;
 
 session_start(); //model,dbの後に記述すること
 require_once SOURCE_BASE . 'partials/header.php';
@@ -25,32 +33,6 @@ $rpath = str_replace('/poll/php/views/', '', CURRENT_URI); //~~.phpの~~のみ�
 $method = strtolower($_SERVER['REQUEST_METHOD']);
 
 route($rpath, $method);
-function route($rpath, $method) {
-    if($rpath === ''){
-        $rpath = 'home';
-    }
-    
-    $targetFile = SOURCE_BASE . "controllers/{$rpath}.php";
-    
-    if(!file_exists($targetFile)){
-        require_once SOURCE_BASE . "views/404.php";
-        return;
-    }
-
-    require_once $targetFile;
-
-    $fn = "controller\\{$rpath}\\{$method}";
-    
-    $fn();
-}
-
-// if($_SERVER['REQUEST_URI'] === '/poll/php/views/login') {
-//     require_once SOURCE_BASE . 'controllers/login.php';
-// } elseif($_SERVER['REQUEST_URI'] === '/poll/php/views/register') {
-//     require_once SOURCE_BASE . 'controllers/register.php';
-// } elseif($_SERVER['REQUEST_URI'] === '/poll/php/views/home') {
-//     require_once SOURCE_BASE . 'controllers/home.php';
-// }
 
 require_once SOURCE_BASE . 'partials/footer.php';
 ?>
--- php/libs/helper.php
+++ php/libs/helper.php
@@ -3,3 +3,13 @@ function get_param($key, $default_val, $is_post = true){
     $arry = $is_post ? $_POST : $_GET;
     return $arry[$key] ?? $default_val;
 }
+
+function redirect($path) {
+    if ($path === GO_HOME) {
+        $path = 'home';
+    } else if ($path === GO_REFERE) {
+        $path = $_SERVER['HTTP_REFERER'];
+    }
+
+    header("Location: {$path}");
+}
